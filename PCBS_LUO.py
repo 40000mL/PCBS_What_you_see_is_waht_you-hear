6972c4a841f66094031af915d8eab1ac6b60b625
JunlianLUO <47482896+40000mL@users.noreply.github.com>
2021-04-14 11:43:10 +0200

    Update PCBS_LUO.py

--- PCBS_LUO.py
+++ PCBS_LUO.py
@@ -9,17 +9,21 @@ Created on Wed Apr  7 10:25:41 2021
 import expyriment;
 import numpy;
 
-audio_duration=100;
+audio_duration=7;
 audio_gap=57;
-stim_variable=[1,2,3,4,5];
+stim_variable=[1,2,3,4];
 visual_gap=50
-visual_duration=audio_duration
+visual_duration=17
+visual_audio_gap=23
+num_of_trials=60
+type_of_trials=4
 
 exp = expyriment.design.Experiment(name="First Experiment")
 expyriment.control.initialize(exp)
 
-audio_number=stim_variable[numpy.random.choice(stim_variable)]
-visual_number=stim_variable[numpy.random.choice(stim_variable)]
+trial_type_num= nympy.random.randint(1,60);
+
+illusion_audio_number=stim_variable[numpy.random.choice(stim_variable[1:]]
 stim_visual=expyriment.stimuli.Circle(50, colour=(255,255,255));
 stim_visual_absence=expyriment.stimuli.Circle(50, colour=(0,0,0));
 stim_audio=expyriment.stimuli.Tone(audio_duration)
@@ -38,20 +42,57 @@ expyriment.control.start()
 #         stim_audio.present()
 #         exp.clock.wait(audio_gap)    
 
-#one flash, multiple beeps
-if visual_number==1:
-    stim_visual.present()
-#    exp.clock.wait(visual_duration)
-    for i in range(audio_number):
+
+# 5 trials per condition, 15 illusionary, 45 non-illusionary
+for k in range(type_of_trials):
+    # illusion trials: one flash, multiple beeps
+    if trial_type_num<(1+k*num_of_trial/type_of_trials):
+      
+        stim_audio.present();
+        exp.clock.wait(visual_audio_gap);
+        stim_visual.present();
+#    ??exp.clock.wait(visual_duration)
+        for i in range(illusion_audio_number-1):
+            stim_audio.present()
+            exp.clock.wait(audio_gap)
+    
+    #multiple flashes, 1 beep or no beep
+    elif trial_type_num<(1+k*num_of_trial/type_of_trials):
+        visual_number=stim_variable[numpy.random.choice(stim_variable)]
+
         stim_audio.present()
-        exp.clock.wait(audio_gap)
-else:
-    for i in range(visual_number):
-        stim_visual.present()
-        exp.clock.wait(visual_duration)
-        stim_visual_absence.present()
-        exp.clock.wait(visual_gap)
+        exp.clock.wait(visual_audio_gap)
+
+        for i in range(visual_number):
+            stim_visual.present()
+            exp.clock.wait(visual_duration)
+            stim_visual_absence.present()
+            exp.clock.wait(visual_gap)
         
+    elif trial_type_num<(1+k*num_of_trial/type_of_trials):
+        visual_number=stim_variable[numpy.random.choice(stim_variable)]
+
+        for i in range(visual_number):
+            stim_visual.present()
+            exp.clock.wait(visual_duration)
+            stim_visual_absence.present()
+            exp.clock.wait(visual_gap)
+        
+    #multiple flashes&beeps
+    else:
+        visual_number=stim_variable[numpy.random.choice(stim_variable)]
+
+        stim_audio.present()
+        exp.clock.wait(visual_audio_gap)
+
+        for i in range(visual_number):
+            stim_visual.present()
+            exp.clock.wait(visual_duration)
+            stim_visual_absence.present()
+            exp.clock.wait(visual_gap)
+
+
+       
 #one beep, multiple flashes
 # if number==1:
 #     stim_audio.present()
